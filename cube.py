6ead5c6e21324209da4690e3057c3e62e613c0d7
CodingCoder-boxgames <staf.van.acker04@gmail.com>
2024-12-24 11:52:28 +0100

    optimization

--- cube.py
+++ cube.py
@@ -99,19 +99,19 @@ while running:
     screen.fill(BLACK)
 
     # code
-    
+    cosA = cos(A)
+    sinA = sin(A)
+    cosB = cos(B)
+    sinB = sin(B)
+    cosC = cos(C)
+    sinC = sin(C)
     output = [' '] * screen_size
     zbuffer = [0] * screen_size
     for x in range(-20, 21):
         for y in range(-20, 21):
             for z in range(-20,21):
                 if x == -20 or x == 20 or y == -20 or y == 20 or z == -20 or z == 20:
-                    cosA = cos(A)
-                    sinA = sin(A)
-                    cosB = cos(B)
-                    sinB = sin(B)
-                    cosC = cos(C)
-                    sinC = sin(C)
+                    
                     xr = cosA * x + sinA * z 
                     yr = y * cosB - (-sinA * x + cosA * z) * sinB
                     zr = y * sinB + (-sinA * x + cosA * z) * cosB
